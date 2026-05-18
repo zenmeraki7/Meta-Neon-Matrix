e1a9420dc992459c953af5af9834eae11cababf0
zenmeraki7 <zenmerakihelp@gmail.com>
2026-05-18 16:44:55 +0530

    Add database config for Heroku runtime

--- .history/web/Config/database_20260518164445.js
+++ .history/web/Config/database_20260518164445.js
@@ -1,6 +1,6 @@
 // FILE: web/config/database.js adding to github
 import { PrismaClient } from "../generated/prisma/index.js";
-// Ensure a single instance of PrismaClient is used across the application
+// 
 const globalForPrisma = globalThis;
 
 export const prisma =
